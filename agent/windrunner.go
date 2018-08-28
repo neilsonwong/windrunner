03b8f4e31959cbf28ec654f7c1d64ecee8ceba14
neilsonwong <hc3wong@engmail.uwaterloo.ca>
2018-08-28 01:47:31 -0400

    updated windrunner to forward ls and thumb to rpi

--- agent/windrunner.go
+++ agent/windrunner.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
+	"strings"
+	"io/ioutil"
 )
 
 const sharename = "//RASPBERRYPI/share"
@@ -12,8 +14,10 @@ func Windunner() {
 	//ensure mount is successful
 	MountSmb(sharename)
 
+	h := http.NewServeMux()
+
 	//setup the play function
-	http.HandleFunc("/play", func(resw http.ResponseWriter, req *http.Request) {
+	h.HandleFunc("/play", func(resw http.ResponseWriter, req *http.Request) {
 		enableCors(&resw)
 
 		switch req.Method {
@@ -28,11 +32,14 @@ func Windunner() {
 		}
 	});
 
-	http.HandleFunc("/hello", func(resw http.ResponseWriter, req *http.Request) {
+	h.HandleFunc("/hello", func(resw http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(resw, "Hello, this is a gopher reporting in")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	hl := proxy(h)
+
+	err := http.ListenAndServe(":8080", hl)
+	log.Fatal(err)
 }
 
 func handlePlay(resw http.ResponseWriter, req *http.Request) {
@@ -43,8 +50,46 @@ func handlePlay(resw http.ResponseWriter, req *http.Request) {
 
 	//perhaps cut the share out of filename in future? not sure
 	Open(sharename, file)
+	fmt.Fprintf(resw, "opened " + file)
 }
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func proxy(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(resw http.ResponseWriter, r *http.Request) {
+		//r.URL is what i want to proxy to the local server
+		urlString := r.URL.String()
+		if strings.HasPrefix(urlString, "/ls") || strings.HasPrefix(urlString, "/thumb") {
+			enableCors(&resw)
+			//handle the proxy request
+			resp, err := proxyRequest(urlString)
+			if err == nil {
+				resw.Write(resp)
+			} else {
+				fmt.Fprintf(resw, "problem proxying to http://192.168.0.159:8000")
+			}
+			return
+		}
+		log.Printf("%s requested %s", r.RemoteAddr, r.URL)
+		h.ServeHTTP(resw, r)
+	})
+}
+
+//it was so close, but https still does not like the sneaky server redirect
+func redir(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("redirecting to " + "http://192.168.0.159:8000"+req.RequestURI)
+	http.Redirect(w, req, "http://192.168.0.159:8000"+req.RequestURI, http.StatusMovedPermanently)
+}
+
+func proxyRequest(url string) ([]byte, error) {
+	resp, err := http.Get("http://192.168.0.159:8000" + url)
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+
 }

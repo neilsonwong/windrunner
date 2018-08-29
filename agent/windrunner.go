fd880b0ba7e6ac74eea0325eb4d60aaf401a3582
neilsonwong <hc3wong@engmail.uwaterloo.ca>
2018-08-28 22:17:11 -0400

    let windrunner forward find requests

--- agent/windrunner.go
+++ agent/windrunner.go
@@ -61,7 +61,7 @@ func proxy(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(resw http.ResponseWriter, r *http.Request) {
 		//r.URL is what i want to proxy to the local server
 		urlString := r.URL.String()
-		if strings.HasPrefix(urlString, "/ls") || strings.HasPrefix(urlString, "/thumb") {
+		if strings.HasPrefix(urlString, "/ls") || strings.HasPrefix(urlString, "/thumb") || strings.HasPrefix(urlString, "/find") {
 			enableCors(&resw)
 			//handle the proxy request
 			resp, err := proxyRequest(urlString)
@@ -84,7 +84,8 @@ func redir(w http.ResponseWriter, req *http.Request) {
 }
 
 func proxyRequest(url string) ([]byte, error) {
-	resp, err := http.Get("http://192.168.0.159:8000" + url)
+	fmt.Println("proxying to http://192.168.0.159:8000" + url)
+	resp, err := http.Get("http://127.0.0.1:8000" + url)
 	if err != nil {
 		// handle error
 	}

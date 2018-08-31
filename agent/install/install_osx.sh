89ec5b54568b02d61c471ee3616aa937ca7d7f10
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2018-08-31 00:14:12 -0400

    fix install on mac

--- agent/install/install_osx.sh
+++ agent/install/install_osx.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # adds agent to osx user profile startup
 
-AGENT="$(pwd)/agent"
-sudo launchctl submit -l "WindrunnerAgent" -- $AGENT 
+# AGENT="$(pwd)/../bin/agent_osx"
+# uninstall old one first
+# sudo launchctl remove "WindrunnerAgent"
+# sudo launchctl submit -l "WindrunnerAgent" -- $AGENT 
+
+# ^^^ is old one, plist should be easier to use
+# we want a launchagent
+
+# copy into launchAgents
+# use cp command as bash one might be aliased
+/bin/cp -rf ./windrunnerAgent.plist ~/Library/LaunchAgents/windrunnerAgent.plist
+
+#fix file permission
+chmod 644 ~/Library/LaunchAgents/windrunnerAgent.plist

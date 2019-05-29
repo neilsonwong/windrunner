b921f12739a5b78bd5b15eb6c7375bf5b6a5c322
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-05-29 08:52:46 -0400

    update osx update script

--- agent/install/update_darwin.sh
+++ agent/install/update_darwin.sh
@@ -7,13 +7,21 @@
 WINDRUNNER_ROOT=$PWD
 
 # ------------------------------------------------------------------------------
+# set the update logging
+# ------------------------------------------------------------------------------
+LOGFILE=$WINDRUNNER_ROOT/update.log
+echo "starting to update" >> $LOGFILE
+
+# ------------------------------------------------------------------------------
 # extract the zip in the update dir
 # ------------------------------------------------------------------------------
+echo "extracting windrunner update $WINDRUNNER_ROOT" >> $LOGFILE
 unzip -o "$WINDRUNNER_ROOT/updates/*.zip" -d "$WINDRUNNER_ROOT/updates/"
 
 # ------------------------------------------------------------------------------
 # stop windrunner agent
 # ------------------------------------------------------------------------------
+echo "stopping windunnner service" >> $LOGFILE
 launchctl stop windrunner.agent
 
 # ------------------------------------------------------------------------------
@@ -24,16 +32,21 @@ rm "$WINDRUNNER_ROOT/agent"
 echo "backup config"
 mv "$WINDRUNNER_ROOT/config.json" "$WINDRUNNER_ROOT/config.json.old"
 echo "copying new files"
-cp "$WINDRUNNER_ROOT/update/agent" "$WINDRUNNER_ROOT/agent"
-cp "$WINDRUNNER_ROOT/update/config.json" "$WINDRUNNER_ROOT/config.json"
+cp "$WINDRUNNER_ROOT/updates/agent" "$WINDRUNNER_ROOT/agent"
+cp "$WINDRUNNER_ROOT/updates/config.json" "$WINDRUNNER_ROOT/config.json"
 #merge is handled by windrunner internally
 
 # ------------------------------------------------------------------------------
 # clear out update dir
 # ------------------------------------------------------------------------------
-rm -rf "$WINDRUNNER_ROOT/updates/*"
+echo "clearing directory $WINDRUNNER_ROOT" >> $LOGFILE
+rm -rf $WINDRUNNER_ROOT/updates/*
 
 # ------------------------------------------------------------------------------
 #start windrunner agent
 # ------------------------------------------------------------------------------
+echo "restarting windunnner service" >> $LOGFILE
 launchctl start windrunner.agent
+
+
+

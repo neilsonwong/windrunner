59722839a1112e84f30c0ad7f15ce53de50cf098
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-08-21 00:13:43 -0400

    added mount checking and fixed auto mounting remote shares

--- server/src/services/cli/mount.js
+++ server/src/services/cli/mount.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs').promises;
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 const config = require('../../../config');
 const logger = require('../../logger');
 
@@ -11,20 +11,29 @@ async function mountRemoteSamba() {
     await fs.mkdir(config.SHARE_PATH, {recursive: true});
 
     // mount the remote samba drive
-    const cmd = `mount -t drvfs '${config.REMOTE_SHARE_PATH}' ${config.SHARE_PATH}`;
+    const cmd = `sudo mount -t drvfs '${config.REMOTE_SHARE_PATH}' ${config.SHARE_PATH}`;
     await new Promise((res, rej) => {
-      exec(cmd, (err, stdout, stderr) => {
-        if (err !== null) {
-          rej(err);
-        }
-        else if (stderr){
-          rej(stderr);
-        }
-        else {
-          logger.info(`mounted ${config.REMOTE_SHARE_PATH} at ${config.SHARE_PATH}`);
-          res(stdout);
+      const child = spawn(cmd);
+      child.stdin.setDefaultEncoding('utf-8');
+      child.stdout.on('data', (data) => {
+        if (data.indexOf('password') > 0) {
+          // i think it's asking for sudo password
+          child.stdin.write(`${config.SUDO_PW}\n`);
+          child.stdin.end();
+          logger.info('we sent in the pw houston');
         }
       });
+
+      child.on('error', (e) => {
+        rej(e);
+      });
+
+      child.on('close', () => {
+        logger.info(`mounted ${config.REMOTE_SHARE_PATH} at ${config.SHARE_PATH}`);
+        res();
+      });
+
+        
     });
   }
   catch (e) {
@@ -33,6 +42,31 @@ async function mountRemoteSamba() {
   }
 }
 
+async function isMounted() {
+  try {
+    const cmd = `mount`;
+    return await new Promise((res, rej) => {
+      exec(cmd, (err, stdout, stderr) => {
+        if (err !== null) {
+          rej(err);
+        }
+        else if (stderr){
+          rej(stderr);
+        }
+        else {
+          res(stdout.indexOf(config.REMOTE_SHARE_PATH) !== -1 &&
+            stdout.indexOf(config.SHARE_PATH) !== -1);
+        }
+      });
+    });
+  }
+  catch (e) {
+    logger.error(`error checking what is mounted`);
+    logger.error(e);
+  }
+}
+
 module.exports = {
-  mountRemoteSamba: mountRemoteSamba
+  mountRemoteSamba: mountRemoteSamba,
+  isMounted: isMounted
 };
--- server/src/services/infra/remoteService.js
+++ server/src/services/infra/remoteService.js
@@ -1,11 +1,18 @@
 'use strict';
 
 const config = require('../../../config');
-const { mountRemoteSamba } = require('../cli/mount');
+const logger = require('../../logger');
+const { isMounted, mountRemoteSamba } = require('../cli/mount');
 
 async function init() {
   if (config.REMOTE_HOST) {
-    await mountRemoteSamba();
+    const alreadyMounted = await isMounted();
+    if (!alreadyMounted) {
+      await mountRemoteSamba();
+    }
+    else {
+      logger.info(`${config.REMOTE_SHARE_PATH} is already mounted at ${config.SHARE_PATH}`);
+    }
   }
 }
 

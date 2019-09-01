2634eac9f85d0803a9d33cbdf0136801fc820f56
Neilson Wong <hc3wong@engmail.uwaterloo.ca>
2019-09-01 00:59:21 -0400

    add fileIndex into fileLibrary

--- server/src/services/data/fileLibraryDbService.js
+++ server/src/services/data/fileLibraryDbService.js
@@ -2,13 +2,21 @@
 
 const logger = require('../../logger');
 const fileLibraryDb = require('./levelDbService').instanceFor('fileLibrary');
+const fileIndexDb = require('./levelDbService').instanceFor('fileLibraryIndex');
 
 async function getFile(file) {
   return await fileLibraryDb.get(file);
 }
 
-async function setFile(file, data) {
-  return await fileLibraryDb.put(file, data);
+async function setFile(fileObj) {
+  if (fileObj.id) {
+    // add id
+    await fileLibraryDb.put(fileObj.id, fileObj);
+    return await fileIndexDb.put(fileObj.path, fileObj.id);
+  }
+  else {
+    logger.error('attempt to setFile without fileId in fileObj');
+  }
 }
 
 async function evictFile(file) {
@@ -16,8 +24,13 @@ async function evictFile(file) {
   return await fileLibraryDb.del(file);
 }
 
+async function findId(filePath) {
+  return await fileIndexDb.get(filePath);
+}
+
 module.exports = {
   get: getFile,
   set: setFile,
   evict: evictFile,
+  findId: findId,
 };
